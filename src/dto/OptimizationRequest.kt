5f655566108563974cf35632b40326addef32cae
Paule <44635962+V3lop5@users.noreply.github.com>
2021-04-24 16:23:38 +0200

    Added general parameters for OptimizationRequest

--- src/dto/OptimizationRequest.kt
+++ src/dto/OptimizationRequest.kt
@@ -4,5 +4,16 @@ data class OptimizationRequest(
     val grid: GridRequest,
 
     // other Parameters
-    val insulationThickness: Double
+    val insulationThickness: Double,
+
+    // cost functions
+    val gridInvestCostTemplate: String, // f(Durchmesser) = y [€/m]
+    val gridOperatingCostTemplate: String, // f(gridInvestCost) = y [€/year]
+    val pumpInvestCostTemplate: String, // f(Leistung) = y [€/kW]
+    val heatGenerationCost: Double, // €/kWh [for calculating heat loss]
+    val lifespanOfResources: Double, // Jahre
+    val wacc: Double, // Weighted Average Cost of Capital in %
+    val electricityCost: Double, // ct/kWh [for pump station]
+    val electricalEfficiency: Double, // for pump
+    val hydraulicEfficiency: Double, // for pump
 )
