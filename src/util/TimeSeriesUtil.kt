688d1c736533bc27e6cf7dee563402a1db06c3db
Paule <44635962+V3lop5@users.noreply.github.com>
2021-04-25 16:56:05 +0200

    Changed loading of csv - fixed build

--- src/util/TimeSeriesUtil.kt
+++ src/util/TimeSeriesUtil.kt
@@ -2,13 +2,13 @@ package de.fhac.ewi.util
 
 import com.github.doyaaaaaken.kotlincsv.dsl.csvReader
 import de.fhac.ewi.model.TemperatureTimeSeries
-import java.io.File
 
-private fun getRessourceFile(file: String) = File(TemperatureTimeSeries::class.java.classLoader.getResource(file)!!.toURI())
+private fun getResourceAsStream(resource: String) =
+    TemperatureTimeSeries::class.java.classLoader.getResourceAsStream(resource)!!
 
 fun loadTemperatureTimeSeries(): List<TemperatureTimeSeries> {
     return csvReader { delimiter = ';' }
-        .readAll(getRessourceFile("temperature-time-series.csv"))
+        .readAll(getResourceAsStream("temperature-time-series.csv"))
         .transpose()
         .map { TemperatureTimeSeries(it.first(), it.drop(1).map(String::toDouble)) }
 }
