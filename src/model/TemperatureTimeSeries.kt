35957d9a85d80c02040b031612a3adafab451abc
Paule <44635962+V3lop5@users.noreply.github.com>
2021-04-27 00:36:37 +0200

    Fixed temperature time series Added 365th element back Added validation Added tests

--- src/model/TemperatureTimeSeries.kt
+++ src/model/TemperatureTimeSeries.kt
@@ -1,3 +1,9 @@
 package de.fhac.ewi.model
 
-data class TemperatureTimeSeries(val key: String, val temperatures: List<Double>)
+data class TemperatureTimeSeries(val key: String, val temperatures: List<Double>) {
+
+    init {
+        if (key.isBlank()) throw IllegalArgumentException("The key for the temperature time series can not be empty.")
+        if (temperatures.size != 365) throw IllegalArgumentException("The time series must contain exactly 365 temperatures.")
+    }
+}
