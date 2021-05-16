71fc1d642654ae508cae05f92e86aebd12d61690
Paule <44635962+V3lop5@users.noreply.github.com>
2021-05-16 23:24:39 +0200

    Improved backend error messages for flow in/return flow templates

--- src/util/ExceptionUtil.kt
+++ src/util/ExceptionUtil.kt
@@ -1,11 +1,11 @@
 package de.fhac.ewi.util
 
-fun <R> catchAndThrowIllegalArgument(block: () -> R): R {
+fun <R> catchAndThrowIllegalArgument(errorMessage: String = "Error parsing input.", block: () -> R): R {
     return try {
         block()
     } catch (e: IllegalArgumentException) {
         throw e
     } catch (e: Exception) {
-        throw IllegalArgumentException("Error parsing input. ${e.message}", e)
+        throw IllegalArgumentException("$errorMessage ${e.message}", e)
     }
 }
