822fe153c4c56fb0faec6345611c5fc045644e31
Paule <44635962+V3lop5@users.noreply.github.com>
2021-06-20 01:44:40 +0200

    Fixed Optimizer

--- src/model/delegate/PipeBasedCalculableDelegate.kt
+++ src/model/delegate/PipeBasedCalculableDelegate.kt
@@ -20,7 +20,7 @@ abstract class PipeBasedCalculableDelegate<T>() : CalculableDelegate<T>() {
     override fun recalculateIndexed(index: Int) = recalculateIndexed(index, connectedPipes)
 
     // Needed to prevent exception due circular call
-    override fun lazyInitialValue() = DoubleArray(8760)
+    override fun lazyInitialValue() = DoubleArray(8760) {999_999.0}
 
     abstract fun recalculateIndexed(index: Int, pipes: List<Pipe>): Double
     abstract fun onPipeConnect(pipe: Pipe)
