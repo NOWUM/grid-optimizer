d4fca2e958da4eca426095c5acc5168df8060eff
Paule <44635962+V3lop5@users.noreply.github.com>
2021-06-20 20:02:58 +0200

    Added critical node (node with the highest pressure loss between it and input) #172

--- src/model/Node.kt
+++ src/model/Node.kt
@@ -43,6 +43,9 @@ abstract class Node(val id: String) {
 
     open val pathToSource: Array<Pipe> by lazy { connectedPipes.single { it.target == this }.let { arrayOf(it, *it.source.pathToSource) } }
 
+    open val criticalChildNode: Node
+        get() = connectedPipes.filter { it.source == this }.maxByOrNull { it.totalPressureLoss.sum() }?.target?:this
+
     fun isParentOf(target: Node): Boolean =
         target in connectedChildNodes || connectedChildNodes.any { it.isParentOf(target) }
 
