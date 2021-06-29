a501f5a439aa48fb8bab4364243b502b39427f16
Paule <44635962+V3lop5@users.noreply.github.com>
2021-06-29 18:57:15 +0200

    Added comments on strategies

--- src/model/strategies/CriticalPathOneByOneReversed.kt
+++ src/model/strategies/CriticalPathOneByOneReversed.kt
@@ -3,8 +3,9 @@ package de.fhac.ewi.model.strategies
 import de.fhac.ewi.model.Optimizer
 
 /**
- * Optimiert nur die Rohrleitungen auf dem kritischen Pfad. Und zwar eine Rohrleitung nach der anderen.
- * Dies wird solange wiederholt, bis kein Update im kritischen Pfad mehr vorgenommen wird.
+ * Strategie dient zur Bestimmung der optimalen Rohrdurchmesser auf dem längsten Pfad.
+ *
+ * Die Leitungen auf dem längste Pfad werden von Einspeisepunkt bis hin zur Entnahmestelle nacheinander optimiert.
  */
 object CriticalPathOneByOneReversed : Strategy {
     override fun apply(optimizer: Optimizer) = with(optimizer) {
