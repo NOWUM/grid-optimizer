09e33ef05a13ca9978fcfb4c234465258fd6dd9e
Paule <44635962+V3lop5@users.noreply.github.com>
2021-06-29 18:37:35 +0200

    Removed loop from critical path strategy

--- src/model/strategies/CriticalPathOneByOneReversed.kt
+++ src/model/strategies/CriticalPathOneByOneReversed.kt
@@ -1,8 +1,6 @@
 package de.fhac.ewi.model.strategies
 
 import de.fhac.ewi.model.Optimizer
-import de.fhac.ewi.model.OutputNode
-import de.fhac.ewi.model.Pipe
 
 /**
  * Optimiert nur die Rohrleitungen auf dem kritischen Pfad. Und zwar eine Rohrleitung nach der anderen.
@@ -11,13 +9,9 @@ import de.fhac.ewi.model.Pipe
 object CriticalPathOneByOneReversed : Strategy {
     override fun apply(optimizer: Optimizer) = with(optimizer) {
         val pipes = grid.criticalPath.reversed()
-        var anyPipeUpdated: Boolean
-        do {
-            anyPipeUpdated = false
-            for (pipe in pipes) {
-                if (optimizePipe(pipe))
-                    anyPipeUpdated = true
-            }
-        } while (anyPipeUpdated)
+
+        for (pipe in pipes) {
+            optimizePipe(pipe)
+        }
     }
 }
