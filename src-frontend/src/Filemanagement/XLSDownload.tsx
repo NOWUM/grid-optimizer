6b7108dad343d50b3ae1554aafa7f5b725f6184e
Christian Gilles <c.gilles@fh-aachen.de>
2021-07-01 23:24:08 +0200

    Download Report in xls

---
+++ src-frontend/src/Filemanagement/XLSDownload.tsx
@@ -0,0 +1,36 @@
+import {Button} from "@material-ui/core";
+import {notify} from "../ReactFlow/Overlays/Notifications";
+import {getFileName, handleDownloadAsFile} from "./FileDownload";
+import React from "react";
+import {baseUrl} from "../utils/utility";
+import {getConfiguration} from "../ReactFlow/OverlayButtons/OptimizeButton";
+import {ResultCode} from "../ReactFlow/FlowContainer";
+
+export const XLSDownload = ({optId}: {optId: string}) => {
+
+    const handleClick = () => {
+        fetchXLS()
+    };
+
+    const fetchXLS = () => {
+            fetch(`${baseUrl}/api/optimize/${optId}/download`, getConfiguration)
+                .then(response => {
+                    if (response.status !== ResultCode.OK) {
+                        response.text().then(text => {
+                            if (text) {
+                                notify(text)
+                            } else {
+                                notify('Unbekannter Fehler beim Aufruf der Optimierungsergebnisse.')
+                            }
+                        });
+                        throw 'Status code not good.';
+                    }
+                    return response.blob();
+                }).then((res) => handleDownloadAsFile(res, getFileName(), "xls"))
+                .catch(e => {
+                    console.error(e)
+                })
+    }
+
+    return <Button onClick={handleClick}>Download XLS</Button>
+}
