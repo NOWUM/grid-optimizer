ef2fb08d7f983656ad930219e0e70c61e7904b91
Paule <paul.krueger@alumni.fh-aachen.de>
2021-07-03 17:43:49 +0200

    Bump ktor from 1.5.4 to 1.6.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,12 +20,12 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.5.0")
     implementation("org.jetbrains.kotlin:kotlin-reflect:1.5.0")
 
-    implementation("io.ktor:ktor-server-netty:1.5.4")
+    implementation("io.ktor:ktor-server-netty:1.6.1")
     implementation("io.ktor:ktor-server-core:1.6.1")
-    implementation("io.ktor:ktor-server-host-common:1.5.4")
-    implementation("io.ktor:ktor-server-sessions:1.5.4")
-    implementation("io.ktor:ktor-gson:1.5.4")
-    testImplementation("io.ktor:ktor-server-tests:1.5.4")
+    implementation("io.ktor:ktor-server-host-common:1.6.1")
+    implementation("io.ktor:ktor-server-sessions:1.6.1")
+    implementation("io.ktor:ktor-gson:1.6.1")
+    testImplementation("io.ktor:ktor-server-tests:1.6.1")
 
     implementation("ch.qos.logback:logback-classic:1.2.1")
 
